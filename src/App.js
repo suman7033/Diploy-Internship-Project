815555d4f869179e5cb0ffc81e4afe5f71b31d03
suman <sumobarnwal@gmail.com>
2024-09-23 20:18:32 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import Page1 from "./component/Page1";
 import Page2 from './component/Page2';
+import Footer from './component/footer';
 
 const App = () => {
   return (
+    <>
     <div>
       <Navbar/>
       <hr/>
-      <Page1/>
+      <Page1/><br/>
       <Page2/>
+      <br/>
       <div className='App-div'>
       <img className='App-img' src='https://printify.com/wp-content/uploads/2020/01/drag.gif'/>
       </div>
+      <Footer/>
     </div>
+    </>
   )
 }
 
---
+++ src/component/footer.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import "./footer.css";
+
+const footer = () => {
+  return (
+    <div>
+       <div class="footer">
+    <div class="footer-container">
+      
+      {/* <div class="footer-logo">
+        <img className="footer-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSauEvj9fbj8GMAaXEcSCNMhB03iDTj2jxJcA&s" alt="Printify Logo"/>
+      </div> */}
+
+      <div class="footer-columns">
+
+        <div class="footer-column">
+          <h3>Integrations</h3>
+          <ul>
+            <li><a href="#">Shopify</a></li>
+            <li><a href="#">Etsy</a></li>
+            <li><a href="#">eBay</a></li>
+            <li><a href="#">Amazon</a></li>
+            <li><a href="#">TikTok Shop</a></li>
+            <li><a href="#">PrestaShop</a></li>
+            <li><a href="#">BigCommerce</a></li>
+            <li><a href="#">Wix</a></li>
+            <li><a href="#">WooCommerce</a></li>
+            <li><a href="#">Squarespace</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-column">
+          <h3>Discover</h3>
+          <ul>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Guides</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Etsy print-on-demand</a></li>
+            <li><a href="#">Shopify print-on-demand</a></li>
+            <li><a href="#">WooCommerce print-on-demand</a></li>
+            <li><a href="#">Wix print-on-demand</a></li>
+            <li><a href="#">Squarespace print-on-demand</a></li>
+            <li><a href="#">Make Your Own Shirt</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-column">
+          <h3>Start Selling</h3>
+          <ul>
+            <li><a href="#">Custom T-shirts</a></li>
+            <li><a href="#">Custom Hoodies</a></li>
+            <li><a href="#">Custom Mugs</a></li>
+            <li><a href="#">Custom Socks</a></li>
+            <li><a href="#">Custom Backpacks</a></li>
+            <li><a href="#">Custom Branding</a></li>
+            <li><a href="#">Sell on Etsy</a></li>
+            <li><a href="#">Sell on Social Media</a></li>
+            <li><a href="#">Free T-shirt Designs</a></li>
+            <li><a href="#">Custom Products</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-column">
+          <h3>Printify</h3>
+          <ul>
+            <li><a href="#">Print on Demand</a></li>
+            <li><a href="#">Print Providers</a></li>
+            <li><a href="#">Experts Program</a></li>
+            <li><a href="#">Printify Express Delivery</a></li>
+            <li><a href="#">Become a Partner</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Printify Quality Promise</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Webinars</a></li>
+            <li><a href="#">Printing Profits Podcast</a></li>
+          </ul>
+        </div>
+      </div>
+
+
+      <div class="footer-social">
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/2048px-Facebook_f_logo_%282021%29.svg.png" alt="Facebook"/></a>
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram"/></a>
+        <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/2496/2496110.png" alt="Twitter"/></a>
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn"/></a>
+        <a href="#"><img src="https://static.vecteezy.com/system/resources/previews/006/057/996/non_2x/tiktok-logo-on-transparent-background-free-vector.jpg" alt="TikTok"/></a>
+      </div>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default footer
